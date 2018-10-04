7f900435f36fa3bd46175fda7b3e78363c900f35
Your Name <you@example.com>
2018-10-04 11:39:25 +0200

    Random bgcolor button

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ if (style.backgroundColor == "white") {
 }
 
 }
+
+function randomColor() {
+    var randomValue = Math.random();
+
+    var style = document.body.style;
+    
+if (randomValue = Math.random() < 0.1) {
+    style.backgroundColor = "yellow";
+} else if (randomValue = Math.random() < 0.2) {
+    style.backgroundColor = "purple";
+} else if (randomValue = Math.random() < 0.3) {
+    style.backgroundColor = "blue";
+} else {
+    style.backgroundColor = "gray";
+}
+}
+
+
