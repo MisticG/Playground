958dfaa23e1c8ae380f03bf20791413b74b51c73
Your Name <you@example.com>
2018-10-04 11:04:31 +0200

    Added chgbg button

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor() {
+
+    var style = document.body.style;
+
+if (style.backgroundColor == "white") {
+    style.backgroundColor = "yellow";
+} else { 
+    style.backgroundColor = "white";
+}
+
+}
